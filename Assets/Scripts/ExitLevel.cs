94a8b6d25a4680bfe48788dc4bb502c4d7a09125
Juan-Majork <juani120805@gmail.com>
2024-09-29 00:27:26 -0300

    puerta
    se a agregado una puerta nueva y su animacion

--- Assets/Scripts/ExitLevel.cs
+++ Assets/Scripts/ExitLevel.cs
@@ -5,11 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class ExitLevel : MonoBehaviour
 {
+
     [SerializeField] private int selecLevel;
 
     private bool hecho = false;
     Animator animator;
-    
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        if (hecho)
+        {
+            animator.SetBool("opening", true);
+            animator.SetBool("open", true);
+        }
+    }
+
     public void openDoor(bool act)
     {
         hecho = act;
